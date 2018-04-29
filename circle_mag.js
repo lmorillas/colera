4c682e22aa9be2f5aa264d199e9e6357001bb7c5
lmorillas <morillas@gmail.com>
2018-04-30 00:18:10 +0200

    Actualización mapas histórico y círculo - issue círculo

---
+++ circle_mag.js
@@ -0,0 +1,16 @@
+eqfeed_callback({
+"type":"FeatureCollection",
+"features":[{
+	"type":"Feature",
+	"id":"0",
+// "mag" changes the diameter of the circle but the zoom is working backwards
+	"properties":{
+	"mag":9.5,
+	"place":"Soho, London",
+	},
+"geometry":{
+	"type":"Point",
+	// lng,lat,depth
+	"coordinates":[-0.136593,51.513338]
+},
+}]});
